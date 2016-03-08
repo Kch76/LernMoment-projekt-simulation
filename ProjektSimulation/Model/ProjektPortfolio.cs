59ab6a13a8cfdefc93175aace2f6504392a3992d
Jan Suchotzki <jan@lernmoment.de>
2016-03-08 14:44:33 +0100

    Erwitert das ProjektPortfolio auf über 20 Einträge, um zu sehen, ob es da Probleme gibt.

--- ProjektSimulation/Model/ProjektPortfolio.cs
+++ ProjektSimulation/Model/ProjektPortfolio.cs
@@ -16,6 +16,23 @@ namespace ProjektSimulation.Model
             projekte.Add(new Projekt("Syntax"));
             projekte.Add(new Projekt("Mail-Client"));
             projekte.Add(new Projekt("Webbrowser"));
+            projekte.Add(new Projekt("Docker"));
+            projekte.Add(new Projekt("Git"));
+            projekte.Add(new Projekt("GitHub"));
+            projekte.Add(new Projekt("WPF"));
+            projekte.Add(new Projekt("MVVM"));
+            projekte.Add(new Projekt("MVC"));
+            projekte.Add(new Projekt("XamarinStudio"));
+            projekte.Add(new Projekt("LernMoment"));
+            projekte.Add(new Projekt("Jekyll"));
+            projekte.Add(new Projekt("Markdown"));
+            projekte.Add(new Projekt("CSS"));
+            projekte.Add(new Projekt("HTML"));
+            projekte.Add(new Projekt("JavaScript"));
+            projekte.Add(new Projekt("Node.JS"));
+            projekte.Add(new Projekt("Awesomium"));
+            projekte.Add(new Projekt("OpenPop"));
+            projekte.Add(new Projekt("UlitmateToolkit"));
 
             return projekte;
         }
