75c23b1b8256877575ae035f5ed156ff80624477
Jan Suchotzki <jan@lernmoment.de>
2016-03-08 13:02:41 +0100

    Fügt eine einfache Version der Klasse Projekt hinzu um zeigen zu können wie diese sich verhält, wenn sie vom Entwickler bearbeitet wird.

---
+++ ProjektSimulation/Model/Projekt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSimulation.Model
+{
+    public enum ProjektStatus
+    {
+        Definition,
+        Planung,
+        Entwicklung,
+        Test,
+        Release
+    }
+
+    public class Projekt
+    {
+        public string Name { get; }
+        public ProjektStatus Status { get; set; }
+
+        public Projekt(string name)
+        {
+            Name = name;
+            Status = ProjektStatus.Definition;
+        }
+
+    }
+}
