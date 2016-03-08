8fd10965a268cb400d9e3fde2ea3032a83b3fe4d
Jan Suchotzki <jan@lernmoment.de>
2016-03-08 13:14:57 +0100

    Fügt die Klasse Entwickler hinzu um zu zeigen wie async auf dem Projekt gearbeitet wird.

---
+++ ProjektSimulation/Model/Entwickler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektSimulation.Model
+{
+    public enum Beschaeftigung
+    {
+        Lernen,
+        Planen,
+        Entwickeln
+    }
+
+    public class Entwickler
+    {
+        public string Name { get; }
+        public Beschaeftigung AktuelleAufgabe { get; private set; }
+
+        public Entwickler(string name)
+        {
+            Name = name;
+            AktuelleAufgabe = Beschaeftigung.Lernen;
+        }
+
+        public async Task Planen(Projekt projekt)
+        {
+            if ((projekt.Status == ProjektStatus.Definition) 
+                || (projekt.Status == ProjektStatus.Test))
+            {
+                projekt.Status = ProjektStatus.Planung;
+                AktuelleAufgabe = Beschaeftigung.Planen;
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                AktuelleAufgabe = Beschaeftigung.Lernen;
+            }
+        }
+
+        public async Task Entwickeln(Projekt projekt)
+        {
+            if (projekt.Status == ProjektStatus.Planung)
+            {
+                projekt.Status = ProjektStatus.Entwicklung;
+                AktuelleAufgabe = Beschaeftigung.Entwickeln;
+                await Task.Delay(TimeSpan.FromSeconds(15));
+                AktuelleAufgabe = Beschaeftigung.Lernen;
+            }
+        }
+    }
+}
