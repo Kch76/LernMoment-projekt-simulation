cfcb9c9117fce7e38ac727191a14476229c4f267
Jan Suchotzki <jan@lernmoment.de>
2016-03-31 21:41:37 +0200

    Erkennt automatisch Änderungen an der Logger-Konfiguration
    Verwendet den ConfigurationTimer um einmal in der Minute die Konfigurationsdatei für die Logging Komponente zu überprüfen.

--- ProjektSimulation/Program.cs
+++ ProjektSimulation/Program.cs
@@ -6,20 +6,28 @@ namespace ProjektSimulation
 {
     static class Program
     {
+        private const string configurationFileName = "LoggingConfig\\ProjektSimulation-Debug-LogConfig.sic";
+        
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            //SiAuto.Si.Enabled = true;
-            SiAuto.Si.LoadConfiguration("LoggingConfig\\ProjektSimulation-Debug-LogConfig.sic");
+            // Logging mit SmartInspect vorbereiten
+            SiAuto.Si.LoadConfiguration(configurationFileName);
+            var timer = new ConfigurationTimer(SiAuto.Si, configurationFileName, 60000);
+
+            // Das eigentliche Ausführen der Anwendung
             SiAuto.Main.EnterProcess("ProjektSimulation");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
             SiAuto.Main.LeaveProcess("ProjektSimulation");
+
+            // Sicherstellen, dass alle Nachrichten aus dem Backlog per Tcp geschrieben werden 
             SiAuto.Main.LogError("Flush Backlog!");
+            // Logging Komponente beenden.
             SiAuto.Si.Dispose();
         }
     }
